d2133ea314334e58526be05a22089f6682ccb489
Luiz Henrique Rapatao <rapatao@rapatao.com>
2024-06-01 16:27:21 +0100

    refactor: move pull request model to a specific file

--- domain/pull_request.go
+++ domain/pull_request.go
@@ -2,19 +2,6 @@ package domain
 
 import "time"
 
-type Config struct {
-	Services []Service `yaml:"services"`
-}
-
-type Service struct {
-	Name         string   `yaml:"name"`
-	Provider     string   `yaml:"provider"`
-	Token        string   `yaml:"token"`
-	Author       string   `yaml:"author"`
-	Repositories []string `yaml:"repositories"`
-	Owner        string   `yaml:"owner"`
-}
-
 type PullRequest struct {
 	Repository     string
 	RepositoryURL  string
